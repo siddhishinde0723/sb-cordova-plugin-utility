ce39ac92bbd82cc9e6169db219f4aca9bbd07b73
swayangjit <swayangjit029@gmail.com>
2024-02-20 19:57:02 +0530

    Issue #SB-000 feat: Added decodeJWTTOKEN feature.

--- www/plugin.js
+++ www/plugin.js
@@ -103,10 +103,10 @@ var sbutility = {
         exec(onSuccess, onError, PLUGIN_NAME, "isGoogleServicesAvailable", ["isGoogleServicesAvailable"])
     },
     getJWTToken: function (key, secret, success, error) {
-        exec(success, error, PLUGIN_NAME, PLUGIN_NAME, ["getJWTToken", key, secret]);
+        exec(success, error, PLUGIN_NAME, "getJWTToken", ["getJWTToken", key, secret]);
     },
     decodeJWTToken: function (token, success, error) {
-        exec(success, error, PLUGIN_NAME, PLUGIN_NAME, ["decodeJWTToken", token]);
+        exec(success, error, PLUGIN_NAME, "decodeJWTToken", ["decodeJWTToken", token]);
     }
 };
 
